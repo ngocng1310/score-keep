ff965d6a690005f8eaeeaf2b32d9d2f93e7244a3
ren <nhungoc.nguyen90@gmail.com>
2017-09-13 17:18:17 +1000

    First commmit

---
+++ client/main.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {Meteor} from 'meteor/meteor';
+import {Players} from './../imports/api/players';
+import {Tracker} from 'meteor/tracker';
+
+
+const renderPlayers = (playerList) => {
+
+  return playerList.map((player) => {
+    return (
+      <p key={player._id}>
+        {player.name} has {player.score} point(s).
+        <button onClick= {() => {
+          Players.update({_id: player._id}, {$inc: {score: 1}});
+        }}>+1</button>
+        <button onClick = {() => {Players.remove({_id: player._id});}}>X</button>
+      </p>
+    );
+  });
+};
+
+const handleSubmit = (e) => {
+  let playerName = e.target.playerName.value;
+  e.preventDefault();
+
+  if (playerName) {
+    e.target.playerName.value = '';
+    Players.insert({
+      name: playerName,
+      score: 14
+    });
+  }
+};
+
+Meteor.startup(() => {
+  Tracker.autorun(() => {
+    let players = Players.find().fetch();
+    let name = 'Mike';
+    let title = 'Score Keep';
+    let jsx = (
+      <div>
+        <h1>{title}</h1>
+        <p>Hello {name}!</p>
+        <p>This is my second p.</p>
+        {renderPlayers(players)}
+        <form onSubmit={handleSubmit}>
+          <input type="text" name="playerName" placeholder="Player name"/>
+          <button>Add Player </button>
+        </form>
+      </div>
+    );
+    ReactDOM.render(jsx, document.getElementById('app'));
+  });
+});
