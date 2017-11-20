f681833c10f31348a664750e0f057e2f1eb3b8ea
ren <nhungoc.nguyen90@gmail.com>
2017-11-21 08:22:21 +1000

    using 3rd party flip move

--- imports/ui/PlayerList.js
+++ imports/ui/PlayerList.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import Player from './Player';
 import PropTypes from 'prop-types';
+import FlipMove from 'react-flip-move';
 
 export default  class PlayerList extends React.Component {
   renderPlayers() {
     if (this.props.players.length === 0) {
       return (
           <div className="item">
-            <p>Add your first player to get started!</p>
+            <p className="item__message">Add your first player to get started!</p>
           </div>
       )
     } else {
@@ -19,7 +20,9 @@ export default  class PlayerList extends React.Component {
   render() {
     return (
       <div>
-        {this.renderPlayers()}
+        <FlipMove maintainContainerHeight={true}>
+            {this.renderPlayers()}
+        </FlipMove>
       </div>
     )
   }
